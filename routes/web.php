cbceefbfbafbb8aae6f86f9544ac98506e3ef7b7
Momen Ahmed <momennoh123aa@gmail.com>
2026-09-05 14:21:54 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware('auth')->group(function () {
 
 
 Route::get('deploy',function () {
-   return 'MOMEN AHMED ';
+   return 'asdgadsfgfadfgdf AHMED ';
 });
 
 Route::get('test-pay',[ProductController::class,'payWithCard']);
